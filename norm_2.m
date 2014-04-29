de5d47bbf75a8866059b89b7569255ab2036ceea
dompaw <paw.dominik@gmail.com>
2014-04-29 16:41:22 +0200

    Skrypt z normalizacja
    W zmiennej A musisz wpisywac z którego folderu ciągnąć dane.
    
    WAŻNE! w excelu nie może być kropka tylko przecinek jako rozdzielnik
    liczb zmiennoprzecinkowych

---
+++ norm_2.m
@@ -0,0 +1,52 @@
+% norm 2
+
+clc, close all, clear all;
+% wczytanie z *.xlsx; 
+obecny_folder = pwd;
+% wczytuje nazwy plików, podaj folder
+A = dir(['pacjent1_lewa' '\' '*.xlsx']);
+cd('pacjent1_lewa')
+for i=1:length(A)
+% do zmiennej dane wczytuje plik z danymi
+dane = xlsread(A(i).name);
+
+[wiersze, kolumny] = size(dane);
+
+for j=1:wiersze
+dane(j,13) = (j/wiersze)*100;
+end
+
+% kolumna D (4) to k¹ty, kolumna L(12) to czas w %
+czas = dane(:,13);
+katy = dane(:,4);
+
+% interpolka sklejanymi
+yy = spline(czas, katy);
+% tworzenie wektora czasu: 
+xx = linspace(czas(1,1),czas(wiersze,1), 1500);
+% wyswietlam na pierwszym subplocie
+% figure(1)
+% plot(xx,ppval(yy,xx))
+% hold on
+% przeliczone dane do wektorów: X to czas znormalizowany; Y to k¹ty po
+% interpolacji
+X(:,i) = xx;
+Y(:,i) = ppval(yy,xx);
+end
+% ustalanie zakresów minimum, maximum, œrednia
+for i =1:1500
+    zbiorczo(i,1) = min(Y(i,:));    % minimum
+    zbiorczo(i,2) = max(Y(i,:));    % maksimum
+    zbiorczo(i,3) = mean(Y(i,:));   % œrednia
+end
+% plotowanie zakresow jako linie
+% plot(xx, zbiorczo(:,1),'b-',xx,zbiorczo(:,2),'b-',xx,zbiorczo(:,3), 'r-')
+
+fill( [xx fliplr(xx)],  [zbiorczo(:,2)' fliplr(zbiorczo(:,1)')], 'k');
+alpha(.25); hold on
+plot(xx,zbiorczo(:,3) , 'k', 'LineWidth', 2)
+plot(xx, zbiorczo(:,2), 'k')
+plot(xx, zbiorczo(:,1), 'k')
+grid on
+
+cd(obecny_folder);
